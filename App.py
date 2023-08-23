6d194f7bf68d6046038b3204ae391f3259798223
Abhishek <114731541+arkulkarni55@users.noreply.github.com>
2023-08-23 20:01:56 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from multiapp import MultiApp
+# from multiapp import MultiApp
 
 app_selection = st.sidebar.selectbox("Utility",("Log_In","Help_Page","File_Upload_Utility","Over_Under_Size_File_Identifier","User_Management","Log_Out"))
 
