dbd2add78f143d8b1db2ed906e8f22ee3a82e667
Abhishek <114731541+arkulkarni55@users.noreply.github.com>
2023-08-23 19:47:22 +0530

    Update App.py

--- App.py
+++ App.py
@@ -3,22 +3,22 @@ import streamlit as st
 
 app_selection = st.sidebar.selectbox("Utility",("Log_In","Help_Page","File_Upload_Utility","Over_Under_Size_File_Identifier","User_Management","Log_Out"))
 
-# from pages import Log_In,Help_Page,File_Upload_Utility,Over_Under_Size_File_Identifier,User_Management,Log_Out
+from pages import Log_In,Help_Page,File_Upload_Utility,Over_Under_Size_File_Identifier,User_Management,Log_Out
 
-# app_functions = {
+app_functions = {
 
-#   "Log_In" : Log_In.app,
-#   "Help_Page": Help_Page.app,
-#   "File_Upload_Utility": File_Upload_Utility.app,
-#   "Over_Under_Size_File_Identifier" : Over_Under_Size_File_Identifier.app,
-#   "User_Management" : User_Management.app ,
-#   "Log_Out" : Log_Out.app
+  "Log_In" : Log_In.app,
+  "Help_Page": Help_Page.app,
+  "File_Upload_Utility": File_Upload_Utility.app,
+  "Over_Under_Size_File_Identifier" : Over_Under_Size_File_Identifier.app,
+  "User_Management" : User_Management.app ,
+  "Log_Out" : Log_Out.app
   
-# }
+}
 
 
-# if app_selection in app_functions:
-#   app_functions[app_selection]()
-# else:
-#   st.error("Apps not found")
+if app_selection in app_functions:
+  app_functions[app_selection]()
+else:
+  st.error("Apps not found")
   
