ddc9d7e8150386d55645f278817acb7d28489070
Abhishek <114731541+arkulkarni55@users.noreply.github.com>
2023-09-04 23:25:51 +0530

    Update App.py

--- App.py
+++ App.py
@@ -36,7 +36,7 @@ st.set_page_config(
 
 
 
-from pages import Log_In,Help_Page,File_Upload_Utility,Over_Under_Size_File_Identifier,User_Management,Log_Out
+#from pages import Log_In,Help_Page,File_Upload_Utility,Over_Under_Size_File_Identifier,User_Management,Log_Out
 
 
 app = MultiApp()
