deb95acc1d608adb6eccb5b4015a956f2ab9e0ea
Abhishek <114731541+arkulkarni55@users.noreply.github.com>
2023-09-05 00:21:39 +0530

    Update App.py

--- App.py
+++ App.py
@@ -17,12 +17,12 @@ from pages import Log_In,Help_Page,File_Upload_Utility,Over_Under_Size_File_Iden
 
 app_functions = {
 
-  "Log_In" : Log_In.page,
-  "Help_Page": Help_Page.page,
-  "File_Upload_Utility": File_Upload_Utility.page,
-  "Over_Under_Size_File_Identifier" : Over_Under_Size_File_Identifier.page,
-  "User_Management" : User_Management.page,
-  "Log_Out" : Log_Out.page
+  "Log_In" : Log_In.app,
+  "Help_Page": Help_Page.app,
+  "File_Upload_Utility": File_Upload_Utility.app,
+  "Over_Under_Size_File_Identifier" : Over_Under_Size_File_Identifier.app,
+  "User_Management" : User_Management.app,
+  "Log_Out" : Log_Out.app
   
 }
 
