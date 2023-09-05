f00a6f409db35c4105e9551017ab5a7f497a791c
Abhishek <114731541+arkulkarni55@users.noreply.github.com>
2023-09-05 19:50:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-
 import streamlit as st
-from multiapp import MultiApp
 from pages import Log_In,Help_Page,File_Upload_Utility,Over_Under_Size_File_Identifier,User_Management,Log_Out
-
 
 st.set_page_config(
   layout="wide",
@@ -10,25 +7,28 @@ st.set_page_config(
 )
 
 
-# app_selection = st.sidebar.selectbox("Utility",("Log_In","Help_Page","File_Upload_Utility","Over_Under_Size_File_Identifier","User_Management","Log_Out"))
+
+app_selection = st.sidebar.selectbox("Utility",("Log_In","Help_Page","File_Upload_Utility","Over_Under_Size_File_Identifier","User_Management","Log_Out"))0
 
 
-app = multiApp()
 
-st.markdown("""
-# Multi-Page App
+app_functions = {
 
-# This multi-page app is using the [streamlit-multiapps](https://github.com/upraneelnihar/streamlit-multiapps) framework developed by [Praneel Nihar](https://medium.com/@u.praneel.nihar). Also check out his [Medium article](https://medium.com/@u.praneel.nihar/building-multi-page-web-app-using-streamlit-7a40d55fa5b4).
+  "Log_In" : Log_In.app,
+  "Help_Page": Help_Page.app,
+  "File_Upload_Utility": File_Upload_Utility.app,
+  "Over_Under_Size_File_Identifier" : Over_Under_Size_File_Identifier.app,
+  "User_Management" : User_Management.app,
+  "Log_Out" : Log_Out.app
+  
+}
 
-""")
 
-# Add all your application here
-app.add_app({"Log_In" : Log_In.app}),
-app.add_app({"Help_Page": Help_Page.app}),
-app.add_app({"File_Upload_Utility": File_Upload_Utility.app}),
-app.add_app({"Over_Under_Size_File_Identifier" : Over_Under_Size_File_Identifier.app}),
-app.add_app({"User_Management" : User_Management.app}),
-app.add_app({"Log_Out" : Log_Out.app})
 
-# The main app
-app.run()
+
+
+if app_selection in app_functions:
+  app_functions[app_selection]()
+else:
+  st.error("Apps not found")
+
