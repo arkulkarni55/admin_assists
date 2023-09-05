70113a6ab3a61ed6709ddbb568838b2ec81e19da
Abhishek <114731541+arkulkarni55@users.noreply.github.com>
2023-09-05 14:23:32 +0530

    Update App.py

--- App.py
+++ App.py
@@ -15,22 +15,22 @@ app_selection = st.sidebar.selectbox("Utility",("Log_In","Help_Page","File_Uploa
 
 
 
-# app_functions = {
+app_functions = {
 
-#   "Log_In" : Log_In.app,
-#   "Help_Page": Help_Page.app,
-#   "File_Upload_Utility": File_Upload_Utility.app,
-#   "Over_Under_Size_File_Identifier" : Over_Under_Size_File_Identifier.app,
-#   "User_Management" : User_Management.app,
-#   "Log_Out" : Log_Out.app
+  "Log_In" : Log_In.app,
+  "Help_Page": Help_Page.app,
+  "File_Upload_Utility": File_Upload_Utility.app,
+  "Over_Under_Size_File_Identifier" : Over_Under_Size_File_Identifier.app,
+  "User_Management" : User_Management.app,
+  "Log_Out" : Log_Out.app
   
-# }
+}
 
 
-# if app_selection in app_functions:
-#   app_functions[app_selection]()
-# else:
-#   st.error("Apps not found")
+if app_selection in app_functions:
+  app_functions[app_selection]()
+else:
+  st.error("Apps not found")
 
 
 
